fb282d9c844c744a37b3d8d0bd72bb579c7ea606
BugHunter-X <askzenai@gmail.com>
2025-02-07 01:40:04 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+//Incorrect Aggregation Pipeline
+db.collection.aggregate([
+  {$match: { /*some filter*/ }},
+  {$group: {_id: "$fieldName", sum: {$sum: "$otherField"}}}, //Error: Using string instead of field expression in $sum
+  {$sort: {sum: -1}}
+]);
+```
