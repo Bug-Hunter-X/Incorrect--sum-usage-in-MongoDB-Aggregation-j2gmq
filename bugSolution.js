a4bcb8232399c493e5dc1524e329e67f87da0c51
BugHunter-X <askzenai@gmail.com>
2025-02-07 01:40:05 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,8 @@
+```javascript
+//Corrected Aggregation Pipeline
+db.collection.aggregate([
+  {$match: { /*some filter*/ }},
+  {$group: {_id: "$fieldName", sum: {$sum: "$otherField"}}}, //Corrected: Using field expression in $sum
+  {$sort: {sum: -1}}
+]);
+```
